a165e1836a64b3c33561dea154c987ee9eb61d49
Roman Ilyshun <romanilyshun@ukr.net>
2022-05-04 18:37:34 +0300

    lab6-10

---
+++ lab6sharp/GCharacter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    internal class GCharacter
+    {
+        public static int[,] FindArc(sMx sMatrix, int[,] matrix)
+        {
+            int n = sMatrix.GetN();
+            int m = sMatrix.GetM();
+
+            int arcOut = 0;
+            int arcIn = 0;
+
+            int[,] result = new int[n + 1, 3];
+
+            for(int i = 1; i < n + 1; i++)
+            {
+                arcOut = 0;
+                arcIn = 0;
+                for(int j = 1; j < n + 1; j++)
+                {
+                    if(matrix[i, j] == 1)
+                    {
+                        arcOut += matrix[i, j];
+                        arcIn += matrix[j, i];
+                    }
+                }
+                result[i, 0] = i;
+                result[i, 1] = arcOut;
+                result[i, 2] = arcIn;
+            }
+            return result;
+        }
+
+        public static int isHomogeneous(sMx sMatrix, int[,] arcMatrix)
+        {
+            for(int i = 1; i < sMatrix.GetN(); i++)
+            {
+                if((i + 1) < sMatrix.GetN())
+                {
+                    if(!(arcMatrix[i, 1] == arcMatrix[i+1, 1]))
+                    {
+                        return 0;
+                    }
+
+                    if(!(arcMatrix[i, 2] == arcMatrix[i + 1, 2]))
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return arcMatrix[1, 1];
+        }
+
+        public static void isIsolated(sMx sMatrix, int[,] arcMatrix)
+        {
+            bool isolated = false;
+            for(int i = 1; i < sMatrix.GetN() + 1; i++)
+            {
+                if(arcMatrix[i, 1] == 0 && arcMatrix[i, 2] == 0)
+                {
+                    Console.WriteLine($"{i, 2} is isolated");
+                    isolated = true;
+                }
+            }
+            if(!isolated)
+            {
+                Console.WriteLine("there is no isolated tops");
+            }
+        }
+
+        public static void isHanging(sMx sMatrix, int[,] arcMatrix)
+        {
+            bool hanging = false;
+            for (int i = 1; i < sMatrix.GetN() + 1; i++)
+            {
+                if (arcMatrix[i, 1] == 1 && arcMatrix[i, 2] == 0)
+                {
+                    Console.WriteLine($"{i,2} is hanging");
+                    hanging = true;
+                }
+            }
+            if (!hanging)
+            {
+                Console.WriteLine("there is no hanging tops");
+            }
+        }
+    }
+}
---
+++ lab6sharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System;
+
+namespace lab6
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            FileS f = new FileS();
+            int[,] result = f.ReadFile();
+
+            iMx matrix = new iMx();
+            sMx sumMatrix = new sMx();
+
+            int[,] IncMatrix = matrix.createMatrix(f, result);
+            int[,] sMatrix = sumMatrix.createMatrix(f, result);
+
+            iMx.printMatrix(IncMatrix, f);
+            Console.WriteLine();
+            sMx.printMatrix(sMatrix, f);
+            
+            f.WriteIncident(IncMatrix);
+            f.WriteSumizhn(sMatrix);
+            
+            //степінь вершини графу 7
+            int[,] arcs = GCharacter.FindArc(sumMatrix, sumMatrix.createMatrix(f, result));
+
+            Console.WriteLine();
+            for (int i = 1; i < f.GetN() + 1; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{arcs[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            GCharacter.isIsolated(sumMatrix, arcs);
+            
+            Console.WriteLine();
+            GCharacter.isHanging(sumMatrix, arcs);
+
+            //Обхід вглиб 8
+            Console.WriteLine();
+            DFS dfs = new DFS();
+            dfs.Search(sumMatrix, sumMatrix.createMatrix(f, result), 8); // з 8 починаю
+            dfs.PrintResult();
+
+            //Обхід вшир 9
+            BFS bfs = new BFS();
+            bfs.Search(sumMatrix, sumMatrix.createMatrix(f, result), 8); // тут також з 8
+            bfs.PrintResult();
+            // 10
+            WeightMatrix wMx = new WeightMatrix();
+            int[,] wMtx = f.ReadFileWeigh();
+            int[,] WMat = wMx.createMatrix(f, wMtx);
+            WeightMatrix.printMatrix(WMat, f);  
+        }
+    }
+}
+
+// 6,7,8,9,10
